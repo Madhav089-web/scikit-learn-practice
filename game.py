f85c46b7d842c88173aed00fc249ef7b886f9813
Madhav Maheshwari <maheshwarimadhav087@gmail.com>
2025-07-21 19:56:40 +0530

    fourth-commit

--- game.py
+++ game.py
@@ -1,12 +1,32 @@
 from ursina import *
 
-app = Ursina()
+app = Ursina("game-testing",icon="charizard.png")
 
 # Add a player cube
-player = Entity(model='cube', color=color.red, position=(0,0,1),size_list=(2,2,2))
+player = Entity(model='cube',  position=(0,0,1),size_list=(5,5,5), texture='brick')
+sphere = Entity(
+    model='sphere',
+    # color=color.azure,
+    scale=2,
+    position=(0, 0, -1),
+    collider='sphere',
+  
+    
+)
+# sphere_group = Entity()
+
+# # Create 3 spheres under the same group
+# for i in range(3):
+#     s = Entity(
+#         model='sphere',
+#         color=color.random_color(),
+#         scale=1,
+#         position=(0, 0, i*2),
+#         parent=sphere_group
+#     )
 
 # Add the floor
-ground = Entity(model='plane', scale=10, color=color.green, collider='box')
+ground = Entity(model='plane', scale=10, color=color.green, collider='box',position=(0,0,0))
 
 camera.position = (0, 0, 20)
 camera.rotation_x = 0
